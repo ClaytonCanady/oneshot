c5c6d43d8fb5d61efb022d5d64e9dc3e8ab44fdd
ClaytonC <cjcanady1113@gmail.com>
2020-11-12 14:39:27 -0500

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header'
+const App = () => {
 	return (
-		<div className='App'>
-			<h1>test</h1>
-		</div>
+		<Router>
+			<Header/>
+			<main className='App'>
+				<h1>test</h1>
+			</main>
+		</Router>
 	);
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+const Header = () => {
+	return (
+		<header className='header'>
+			<Navbar expand='lg' variant='dark' collapseOnSelect>
+				<Container>
+					<LinkContainer to='/'>
+						<Navbar.Brand>
+							One Shot Ideas 
+						</Navbar.Brand>
+					</LinkContainer>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='px-5'>
+							<LinkContainer to='/one-shots'>
+								<Nav.Link>One Shots</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/monsters'>
+								<Nav.Link>Monsters</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/classes'>
+								<Nav.Link>Classes</Nav.Link>
+							</LinkContainer>
+
+							<LinkContainer to='/forum'>
+								<Nav.Link>Forum</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link>Login/Signup</Nav.Link>
+							</LinkContainer>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
+
+export default Header;
