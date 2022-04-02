5281163ded24a9b0d6a0efa53265a1bbcce05c6d
Clayton C <67292469+ClaytonCanady@users.noreply.github.com>
2022-04-02 16:50:12 -0500

    edit models to be unique

--- backend/oneshot/entries/models.py
+++ backend/oneshot/entries/models.py
@@ -1,20 +1,63 @@
 from django.db import models
+from django.db.models import IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class entry(models.Model):
-    ONESHOT = 'OS'
-    CLASS = 'CL'
-    CAMPAIGN = 'CA'
-    MONSTER = 'MO'
-    TYPE_CHOICES = [
-        (ONESHOT, 'Oneshot'),
-        (CLASS, 'Class'),
-        (CAMPAIGN, 'Campaign'),
-        (MONSTER, 'Monster'),
-    ]
-    TYPE = models.CharField(
-        max_length=2,
-        choices=TYPE_CHOICES,
-        default=ONESHOT,
-    )
+class oneshot(models.Model):
     name = models.CharField(max_length=50)
+    desc = models.TextField()
+
+class campaign_setting(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    
+
+class character_class(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+
+
+class monster(models.Model):
+    name = models.CharField(max_length=50)
+    strength = IntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ]
+    )
+    deterity =IntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ]
+    )
+    constitution = IntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ]
+    )
+    intelligence = IntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ]
+    )
+    wisdom = IntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ]
+    )
+    charisma = IntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ]
+    )
     desc = models.TextField()
